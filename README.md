77c88de6c083efffd78d4954ea0e37c799e5f711
Fedor Urvanov <fedor@urvanov.ru>
2019-01-20 15:40:12 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+I created my first WordPress Plugin!!
+See the development guide in
+https://urvanov.ru
---
+++ urvanov-wordpress-plugin-0001/urvanov-wordpress-plugin-0001.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Urvanov WordPress Plugin 0001
+Plugin URI: https://urvanov.ru
+Description: This is my first educational plugin.
+Author: Fedor Urvanov
+Version: 0.1
+Author URI: https://urvanov.ru
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+add_action( 'wp_head', 'my_addition_to_head');
+
+function my_addition_to_head() {
+    print('<!-- Urvanov Wordpress addition to head -->');
+}
+?>
